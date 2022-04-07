1d0c974efba43b0fad16b8f146de6e3cdb5e03e3
Jimmy Chiu <z42442412@gmail.com>
2022-04-07 15:57:25 +0800

    Part3 update

---
+++ MVCProject/Part3/WebCoreLab/WebCoreLab/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCoreLab.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
